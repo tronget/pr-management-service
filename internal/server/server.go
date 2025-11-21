7ca79f2c83ad0e7d08393843bf25cc545676bd2f
tronget <andrejlashkul@gmail.com>
2025-11-21 18:20:46 +0300

    feat: add server implementation

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,40 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/tronget/pr-management-service/internal/config"
+)
+
+type Server interface {
+	Start() error
+}
+
+type server struct {
+	cfg    *config.Config
+	dbPool *pgxpool.Pool
+}
+
+func New(cfg *config.Config, dbPool *pgxpool.Pool) Server {
+	return &server{
+		cfg:    cfg,
+		dbPool: dbPool,
+	}
+}
+
+func (s *server) Start() error {
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	return http.ListenAndServe(s.cfg.HTTP.Address, r)
+}
