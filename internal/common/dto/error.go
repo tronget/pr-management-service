9f8e05df513af56a3e8e2295936a902d6009a48c
tronget <andrejlashkul@gmail.com>
2025-11-22 17:10:47 +0300

    feat(errors): ErrorCode implements Error interface + new ErrorCode constants

--- internal/common/dto/error.go
+++ internal/common/dto/error.go
@@ -2,13 +2,19 @@ package dto
 
 type ErrorCode string
 
+func (e ErrorCode) Error() string {
+	return string(e)
+}
+
 const (
-	ErrTeamExists  ErrorCode = "TEAM_EXISTS"
-	ErrPRExists    ErrorCode = "PR_EXISTS"
-	ErrPRMerged    ErrorCode = "PR_MERGED"
-	ErrNotAssigned ErrorCode = "NOT_ASSIGNED"
-	ErrNoCandidate ErrorCode = "NO_CANDIDATE"
-	ErrNotFound    ErrorCode = "NOT_FOUND"
+	ErrTeamExists     ErrorCode = "TEAM_EXISTS"
+	ErrPRExists       ErrorCode = "PR_EXISTS"
+	ErrPRMerged       ErrorCode = "PR_MERGED"
+	ErrNotAssigned    ErrorCode = "NOT_ASSIGNED"
+	ErrNoCandidate    ErrorCode = "NO_CANDIDATE"
+	ErrNotFound       ErrorCode = "NOT_FOUND"
+	ErrInternalServer ErrorCode = "INTERNAL_SERVER_ERROR"
+	ErrBadRequest     ErrorCode = "BAD_REQUEST"
 )
 
 type ErrorResponse struct {
@@ -18,4 +24,13 @@ type ErrorResponse struct {
 type ErrorPayload struct {
 	Code    ErrorCode `json:"code"`
 	Message string    `json:"message"`
+}
+
+func NewErrorResponse(code ErrorCode, message string) *ErrorResponse {
+	return &ErrorResponse{
+		Error: ErrorPayload{
+			Code:    code,
+			Message: message,
+		},
+	}
 }
